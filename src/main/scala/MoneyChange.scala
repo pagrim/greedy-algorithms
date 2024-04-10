3a4514019d4c6724825c46e2708f5a746b2fb9d3
Peter Grimshaw <pagrimshaw@gmail.com>
2024-04-10 08:39:28 +0100

    Simplified MoneyChange

--- src/main/scala/MoneyChange.scala
+++ src/main/scala/MoneyChange.scala
@@ -3,30 +3,26 @@ import scala.io.StdIn.readInt
 
 object MoneyChange {
 
-  def solve(targetVal: Int): Array[Int] = {
+  val coinSet: Array[Int] = Array(1,5,10)
 
-    val coinSet = Array(1,5,10)
-
-    @tailrec
-    def _solve(coins: Array[Int]): Array[Int] = {
-      val currVal = coins.zip(coinSet).foldLeft(0)( (acc, coinPair) => acc + coinPair._1 * coinPair._2)
-      val missingVal = targetVal - currVal
-      if(missingVal == 0) {
-        coins
-      } else {
-        val maxIndex = coinSet.zipWithIndex.filter(coinIdx => coinIdx._1 <= missingVal).maxBy(
-          coinIdx => coinIdx._1)._2
-        val newCoins = coins.updated(maxIndex, coins(maxIndex) + 1)
-        _solve(newCoins)
-      }
+  @tailrec
+  def _solve(coins: Array[Int], targetVal: Int): Array[Int] = {
+    val currVal = coins.zip(coinSet).foldLeft(0)( (acc, coinPair) => acc + coinPair._1 * coinPair._2)
+    val missingVal = targetVal - currVal
+    if(missingVal == 0) {
+      coins
+    } else {
+      val maxIndex = coinSet.zipWithIndex.filter(coinIdx => coinIdx._1 <= missingVal).maxBy(
+        coinIdx => coinIdx._1)._2
+      val newCoins = coins.updated(maxIndex, coins(maxIndex) + 1)
+      _solve(newCoins, targetVal)
     }
-    val initCoins = Array.fill(coinSet.length)(0)
-    _solve(initCoins)
-
   }
 
   def calcMinCoins(targetVal: Int): Int = {
-    val coinsNeeded = solve(targetVal)
+
+    val initCoins = Array.fill(coinSet.length)(0)
+    val coinsNeeded = _solve(initCoins, targetVal)
     coinsNeeded.sum
   }
 
